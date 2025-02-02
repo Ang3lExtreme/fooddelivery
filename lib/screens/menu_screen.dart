f5da8415cee3fa80e4b55ad4defc4b9e9241bc7e
Fred <frednevesal@gmail.com>
2025-02-02 20:57:02 +0000

    - testing menu

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'cart_screen.dart';
+import 'home_screen.dart';
+import 'login_screen.dart';
+import 'profile_screen.dart';
+import 'register_screen.dart';
+import 'search_screen.dart';
+
 class MenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -7,8 +14,63 @@ class MenuScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Menu'),
       ),
-      body: Center(
-        child: Text('Welcome to the Menu Screen!'),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Profile'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfileScreen()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Search'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SearchScreen()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Login'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Cart'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CartScreen()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Home'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Register'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => RegisterScreen()),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
